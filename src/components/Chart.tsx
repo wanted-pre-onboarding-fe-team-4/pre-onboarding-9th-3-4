68d717b677017704692c22a753d670aaea5035ea
송경환 <as5427072@gmail.com>
2023-03-16 13:57:33 +0900

    [#4] 차트 구현 (#5)

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from 'react';
+import {
+  ComposedChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+  Bar,
+  Area,
+} from 'recharts';
 import getData from '../api/getData';
+import { ChartStyleConfig } from '../config/ChartStyleConfig';
 import { IChartData } from '../types/dataType';
 
 const Chart = () => {
-  const [chartData, setChartData] = useState<IChartData[] | undefined>([]);
+  const [chartData, setChartData] = useState<IChartData[]>();
 
   useEffect(() => {
     getData().then((res) => {
@@ -11,9 +22,65 @@ const Chart = () => {
     });
   }, []);
 
-  console.log(chartData);
+  return (
+    <ComposedChart
+      width={ChartStyleConfig.CHART_WIDTH}
+      height={ChartStyleConfig.CHART_HEIGHT}
+      data={chartData}
+      margin={ChartStyleConfig.CHART_MARGIN}
+    >
+      <XAxis
+        dataKey='date'
+        label={{
+          value: '2023-02-05일자',
+          position: 'insideBottomLeft',
+          offset: -10,
+        }}
+        minTickGap={100}
+      />
+      <YAxis
+        dataKey='value_area'
+        yAxisId='area'
+        domain={[0, 200]}
+        label={{
+          value: 'area',
+          angle: -90,
+          position: 'insideLeft',
+        }}
+      />
+      <YAxis
+        dataKey='value_bar'
+        yAxisId='bar'
+        orientation='right'
+        label={{
+          value: 'bar',
+          angle: -90,
+          offset: -10,
+          position: 'insideRight',
+        }}
+      />
 
-  return <></>;
+      <Tooltip />
+      <Legend align='center' />
+      <CartesianGrid stroke='#f5f5f5' />
+
+      <Bar
+        dataKey='value_bar'
+        yAxisId='bar'
+        barSize={20}
+        fill={ChartStyleConfig.BAR_FILL_COLOR}
+        stroke={ChartStyleConfig.BAR_STROKE_COLOR}
+      />
+      <Area
+        type='monotone'
+        yAxisId='area'
+        dataKey='value_area'
+        fill={ChartStyleConfig.AREA_FILL_COLOR}
+        fillOpacity={0.8}
+        stroke={ChartStyleConfig.AREA_STROKE_COLOR}
+      ></Area>
+    </ComposedChart>
+  );
 };
 
 export default Chart;
---
+++ src/config/ChartStyleConfig.ts
@@ -0,0 +1,9 @@
+export const ChartStyleConfig = {
+  CHART_WIDTH: 1600,
+  CHART_HEIGHT: 800,
+  CHART_MARGIN: { top: 20, right: 30, left: 30, bottom: 0 },
+  BAR_FILL_COLOR: '#9ea1ff',
+  BAR_STROKE_COLOR: '#9ea1ff',
+  AREA_FILL_COLOR: '#ff7875',
+  AREA_STROKE_COLOR: '#ff7875',
+};
