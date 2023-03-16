55cf9587cae0c3ce55ccdcf718081b3d84283d20
세현 <104138055+sehyeon4687@users.noreply.github.com>
2023-03-16 18:10:38 +0900

    [#6] hover할때 custom tooltip과 highlight 기능 추가 (#7)

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Key, useEffect, useState } from 'react';
 import {
   ComposedChart,
   XAxis,
@@ -8,13 +8,16 @@ import {
   CartesianGrid,
   Bar,
   Area,
+  Cell,
 } from 'recharts';
 import getData from '../api/getData';
-import { ChartStyleConfig } from '../config/ChartStyleConfig';
+import { activeDot, ChartStyleConfig } from '../config/ChartStyleConfig';
 import { IChartData } from '../types/dataType';
+import CustomTooltip from './CustomTooltip';
 
 const Chart = () => {
   const [chartData, setChartData] = useState<IChartData[]>();
+  const [hover, setHover] = useState('');
 
   useEffect(() => {
     getData().then((res) => {
@@ -22,12 +25,24 @@ const Chart = () => {
     });
   }, []);
 
+  const handleHover = (e: any) => {
+    const { isTooltipActive, activePayload } = e;
+    const dataExist = isTooltipActive && activePayload;
+
+    if (dataExist) {
+      const date = activePayload[0].payload.date;
+      setHover(date);
+    }
+  };
+
   return (
     <ComposedChart
       width={ChartStyleConfig.CHART_WIDTH}
       height={ChartStyleConfig.CHART_HEIGHT}
       data={chartData}
       margin={ChartStyleConfig.CHART_MARGIN}
+      onMouseMove={handleHover}
+      onMouseLeave={() => setHover('')}
     >
       <XAxis
         dataKey='date'
@@ -60,7 +75,7 @@ const Chart = () => {
         }}
       />
 
-      <Tooltip />
+      <Tooltip content={<CustomTooltip />} />
       <Legend align='center' />
       <CartesianGrid stroke='#f5f5f5' />
 
@@ -68,9 +83,20 @@ const Chart = () => {
         dataKey='value_bar'
         yAxisId='bar'
         barSize={20}
-        fill={ChartStyleConfig.BAR_FILL_COLOR}
         stroke={ChartStyleConfig.BAR_STROKE_COLOR}
-      />
+      >
+        {chartData &&
+          chartData.map((el: { date: Key | null | undefined }) => (
+            <Cell
+              key={el.date}
+              fill={
+                el.date === hover
+                  ? ChartStyleConfig.BAR_HIGHRIGHT_COLOR
+                  : ChartStyleConfig.BAR_FILL_COLOR
+              }
+            />
+          ))}
+      </Bar>
       <Area
         type='monotone'
         yAxisId='area'
@@ -78,7 +104,8 @@ const Chart = () => {
         fill={ChartStyleConfig.AREA_FILL_COLOR}
         fillOpacity={0.8}
         stroke={ChartStyleConfig.AREA_STROKE_COLOR}
-      ></Area>
+        activeDot={activeDot}
+      />
     </ComposedChart>
   );
 };
---
+++ src/components/CustomTooltip.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const CustomTooltip = ({ active, payload }: any) => {
+  const valid = active && payload?.length;
+
+  if (!valid) {
+    return null;
+  }
+
+  const { id, value_area, value_bar, date } = payload[0].payload;
+
+  return (
+    <TooltipStyle>
+      <p>ID: {id}</p>
+      <p>Area: {value_area}</p>
+      <p>Bar: {value_bar}</p>
+      <p>Date: {date}</p>
+    </TooltipStyle>
+  );
+};
+
+export default CustomTooltip;
+
+const TooltipStyle = styled.div`
+  background-color: white;
+  border: 3px solid #8884d8;
+  padding: 0 10px;
+`;
