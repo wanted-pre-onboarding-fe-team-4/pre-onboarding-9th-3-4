9cad55f4c1a6e721bb1bcd8bb9c88577cbcf763b
SongNoin <as5427072@gmail.com>
2023-03-16 13:53:33 +0900

    fix 차트 스타일 상수 파일 분리

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -10,11 +10,8 @@ import {
   Area,
 } from 'recharts';
 import getData from '../api/getData';
+import { ChartStyleConfig } from '../config/ChartStyleConfig';
 import { IChartData } from '../types/dataType';
-
-const CHART_WIDTH = 1600;
-const CHART_HEIGHT = 800;
-const CHART_MARGIN = { top: 20, right: 30, left: 30, bottom: 0 };
 
 const Chart = () => {
   const [chartData, setChartData] = useState<IChartData[]>();
@@ -27,10 +24,10 @@ const Chart = () => {
 
   return (
     <ComposedChart
-      width={CHART_WIDTH}
-      height={CHART_HEIGHT}
+      width={ChartStyleConfig.CHART_WIDTH}
+      height={ChartStyleConfig.CHART_HEIGHT}
       data={chartData}
-      margin={CHART_MARGIN}
+      margin={ChartStyleConfig.CHART_MARGIN}
     >
       <XAxis
         dataKey='date'
@@ -71,16 +68,16 @@ const Chart = () => {
         dataKey='value_bar'
         yAxisId='bar'
         barSize={20}
-        fill='#9ea1ff'
-        stroke='#9ea1ff'
+        fill={ChartStyleConfig.BAR_FILL_COLOR}
+        stroke={ChartStyleConfig.BAR_STROKE_COLOR}
       />
       <Area
         type='monotone'
         yAxisId='area'
         dataKey='value_area'
-        fill='#ff7875'
+        fill={ChartStyleConfig.AREA_FILL_COLOR}
         fillOpacity={0.8}
-        stroke='#ff7875'
+        stroke={ChartStyleConfig.AREA_STROKE_COLOR}
       ></Area>
     </ComposedChart>
   );
