15a743ccde3bcf426f1d75c69cc6fa85b0657576
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-17 20:00:51 +0900

    [#11] 스타일링 및 반응형 추가 (#16)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense, useState } from 'react';
+import styled from 'styled-components';
 import getData from './api/getData';
 import Chart from './components/Chart';
 import { Filter } from './components/Filter';
@@ -13,18 +14,44 @@ function App() {
   );
 
   return (
-    <div className='App'>
-      <h1>Flexys Chart</h1>
-      <Filter setFilterFunction={setFilterFunction} />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Chart
-          getChartData={getChartData}
-          filterFunction={filterFunction}
-          setFilterFunction={setFilterFunction}
-        />
-      </Suspense>
-    </div>
+    <Wrapper>
+      <Title>Flexys Chart</Title>
+      <Filter
+        filterFunction={filterFunction}
+        setFilterFunction={setFilterFunction}
+      />
+      <ChartWrapper>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Chart
+            getChartData={getChartData}
+            filterFunction={filterFunction}
+            setFilterFunction={setFilterFunction}
+          />
+        </Suspense>
+      </ChartWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 0 1rem;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #ededf4;
+`;
+
+const Title = styled.h1`
+  color: #373737;
+`;
+
+const ChartWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default App;
---
+++ src/types/ButtonType.ts
@@ -0,0 +1,3 @@
+export interface ButtonType {
+  active: boolean;
+}
