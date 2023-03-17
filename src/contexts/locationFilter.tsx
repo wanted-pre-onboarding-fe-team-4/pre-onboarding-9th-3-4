7eb0ce73033493c5584c91460c4d06aefedf0944
Seongbin Kim <seongbin9786@gmail.com>
2023-03-17 21:57:00 +0900

    refactor: 상수화

--- src/contexts/locationFilter.tsx
+++ src/contexts/locationFilter.tsx
@@ -1,6 +1,8 @@
 import { createContext, PropsWithChildren, useContext, useState } from 'react';
 import { IChartData } from '../types/dataType';
 
+const LOCATION_RESET = '해제';
+
 const defaultLocationFilterContext = {
   locationFilter: (data: IChartData) => !!data,
   changeLocation: (loc: string) => {
@@ -13,22 +15,22 @@ const LocationFilterContext = createContext(defaultLocationFilterContext);
 export const useLocationFilter = () => useContext(LocationFilterContext);
 
 export const LocationFilterProvider = (props: PropsWithChildren) => {
-  const [filteredLocation, setFilterLocation] = useState('해제');
+  const [filteredLocation, setFilterLocation] = useState(LOCATION_RESET);
 
   const changeLocation = (curLocation: string) => {
     const prevLocation = filteredLocation;
     const dobuleToggle = curLocation === prevLocation;
-    const reset = curLocation === '해제';
+    const reset = curLocation === LOCATION_RESET;
     const filterRemoved = dobuleToggle || reset;
     if (filterRemoved) {
-      setFilterLocation('해제');
+      setFilterLocation(LOCATION_RESET);
       return;
     }
     setFilterLocation(curLocation);
   };
 
   const locationFilter = ({ id }: IChartData) => {
-    if (filteredLocation === '해제') {
+    if (filteredLocation === LOCATION_RESET) {
       return false;
     }
     return filteredLocation === id;
